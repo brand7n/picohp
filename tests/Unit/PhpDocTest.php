c7f337dc60c9a98d865a4e2b1a38a334d64d9988
Brandin L Claar <brandin@remodulate.com>
2024-12-30 11:48:37 -0500

    add phpdoc parser

---
+++ tests/Unit/PhpDocTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use PHPStan\PhpDocParser\Lexer\Lexer;
+use PHPStan\PhpDocParser\ParserConfig;
+use PHPStan\PhpDocParser\Parser\ConstExprParser;
+use PHPStan\PhpDocParser\Parser\PhpDocParser;
+use PHPStan\PhpDocParser\Parser\TokenIterator;
+use PHPStan\PhpDocParser\Parser\TypeParser;
+
+it('parses a PHPDocs', function () {
+    // basic setup
+
+    $config = new ParserConfig(usedAttributes: []);
+    $lexer = new Lexer($config);
+    $constExprParser = new ConstExprParser($config);
+    $typeParser = new TypeParser($config, $constExprParser);
+    $phpDocParser = new PhpDocParser($config, $typeParser, $constExprParser);
+
+    // parsing and reading a PHPDoc string
+
+    $tokens = new TokenIterator($lexer->tokenize('/** @var int $a */'));
+    $phpDocNode = $phpDocParser->parse($tokens); // PhpDocNode
+    $varTags = $phpDocNode->getVarTagValues(); // ParamTagValueNode[]
+
+    expect($varTags[0]->variableName)->toBe('$a');
+    expect($varTags[0]->type->__toString())->toBe('int');
+});
