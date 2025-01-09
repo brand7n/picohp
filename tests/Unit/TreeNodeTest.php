d82f21e16428260235967653a8f479377469958a
Brandin L Claar <brandin@remodulate.com>
2025-01-09 14:26:34 -0500

    forgot pint

--- tests/Unit/TreeNodeTest.php
+++ tests/Unit/TreeNodeTest.php
@@ -26,23 +26,19 @@ class TestNode implements NodeInterface
 }
 
 it('can manipulate a tree with the tree trait', function () {
-	$root = new TestNode('root');
-	$child1 = new TestNode('child1');
-	$child2 = new TestNode('child2');
+    $root = new TestNode('root');
+    $child1 = new TestNode('child1');
+    $child2 = new TestNode('child2');
 
-	$root->addChild($child1);
-	$root->addChild($child2);
+    $root->addChild($child1);
+    $root->addChild($child2);
 
-	expect($root->getValue())->toBe('root');
-	expect($child1->getValue())->toBe('child1'); 
-	expect($child2->getValue())->toBe('child2'); 
+    expect($root->getValue())->toBe('root');
+    expect($child1->getValue())->toBe('child1');
+    expect($child2->getValue())->toBe('child2');
 
-	expect($root->hasChildren())->toBeTrue();
-	$parent = $child1->getParent();
-	assert($parent instanceof TestNode);
-	expect($parent->getValue())->toBe('root');
+    expect($root->hasChildren())->toBeTrue();
+    $parent = $child1->getParent();
+    assert($parent instanceof TestNode);
+    expect($parent->getValue())->toBe('root');
 });
- 
-
-
-
