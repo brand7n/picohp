f2e90f268b22a0b0728a305b5cf4b9441493ef7a
Brandin L Claar <brandin@remodulate.com>
2025-01-09 14:20:05 -0500

    add sample file for build test

---
+++ examples/example1.php
@@ -0,0 +1,11 @@
+<?php
+
+function main(): int
+{
+    /** @var int */
+    $a = 5 + 4 * 3;
+    /** @var int */
+    $b = $a;
+    echo($b);
+    return $a / 2;
+}
--- tests/Feature/BuildTest.php
+++ tests/Feature/BuildTest.php
@@ -5,5 +5,5 @@ declare(strict_types=1);
 
 it('builds a picoHP program', function () {
     /** @phpstan-ignore-next-line */
-    $this->artisan('build test.php')->assertExitCode(0);
+    $this->artisan('build examples/example1.php')->assertExitCode(0);
 });
