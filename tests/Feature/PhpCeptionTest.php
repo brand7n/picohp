69cde2f9388142f55169f03ce1f7eab39ff06247
Brandin L Claar <brandin@remodulate.com>
2025-01-15 12:35:43 -0500

    use the actual build path

--- tests/Feature/PhpCeptionTest.php
+++ tests/Feature/PhpCeptionTest.php
@@ -7,7 +7,11 @@ it('calls a picoHP lib from PHP', function () {
     /** @phpstan-ignore-next-line */
     $this->artisan('build examples/php_ception.php --shared-lib --out=ffitest.so')->assertExitCode(0);
 
-    $ffi = FFI::cdef("int ffitest();", "./build/ffitest.so");
+    $buildPath = config('app.build_path');
+    assert(is_string($buildPath));
+
+    $ffi = FFI::cdef("int ffitest();", "{$buildPath}/ffitest.so");
+
     /** @phpstan-ignore-next-line */
     expect($ffi->ffitest())->toBe(49);
 });
